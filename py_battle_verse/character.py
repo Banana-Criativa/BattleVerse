64fff920ffbac3115b9289154e2f79bca2f4f275
Radetic <danielradetic@gmail.com>
2022-02-17 20:57:57 -0300

    Introducing mockup for both textual representations of Character objects

--- py_battle_verse/character.py
+++ py_battle_verse/character.py
@@ -18,3 +18,15 @@ class Character:
 		self.name = kwargs.get('name', 'Nanashi')
 		self.birth_date = kwargs.get('birth', datetime.now() - timedelta(days=366*18))
 		self.development = []
+	
+	def __str__(self):
+		profile = "\n".join( [
+			"\n\tName: " + self.name,
+			"\tBirthday: " + self.birth_date.strftime("%d/%m/%Y"),
+			"\tDevelopment: " + str(self.development) + "\n"
+		] )
+		
+		return profile
+	
+	def __repr__(self):
+		return "[{0}] {1} Ki:{2}".format("Human", self.name, 5)
